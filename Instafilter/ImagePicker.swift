d4636525fa42cc1f9e2a1aec9453ef4840270b2c
MarcusStilwell <marcusstilwell@icloud.com>
2021-11-11 14:58:58 -0700

    Added button to allow user to select a photo from their library. This was done by wrapping a UIViewController because SwiftUI cannot perform said action.

---
+++ Instafilter/ImagePicker.swift
@@ -0,0 +1,20 @@
+//
+//  ImagePicker.swift
+//  Instafilter
+//
+//  Created by Marcus Stilwell on 11/11/21.
+//
+
+import Foundation
+import SwiftUI
+
+struct ImagePicker: UIViewControllerRepresentable {
+    func makeUIViewController(context: Context) -> UIImagePickerController {
+        let picker = UIImagePickerController()
+        return picker
+    }
+    
+    func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {
+        
+    }
+}
